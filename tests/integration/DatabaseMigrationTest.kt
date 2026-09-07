28637284a855938a4e7580058ad606a2845e0b26
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-07 20:23:17 -0300

    test: expect organization lifecycle migration

--- tests/integration/DatabaseMigrationTest.kt
+++ tests/integration/DatabaseMigrationTest.kt
@@ -76,7 +76,7 @@ class DatabaseMigrationTest {
                 ).use { statement ->
                     statement.executeQuery().use { result ->
                         result.next()
-                        assertEquals(18, result.getInt(1))
+                        assertEquals(19, result.getInt(1))
                     }
                 }
             }
