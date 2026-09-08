3d6cd47097229f0d5141c40044040bb2fa318b23
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-08 13:22:29 -0300

    test: add browser api mocks

--- web/tests/setup.ts
+++ web/tests/setup.ts
@@ -3,3 +3,25 @@ import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
 
 afterEach(cleanup);
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: () => undefined,
+    removeEventListener: () => undefined,
+    addListener: () => undefined,
+    removeListener: () => undefined,
+    dispatchEvent: () => false,
+  }),
+});
+
+class ResizeObserverMock {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+Object.defineProperty(window, "ResizeObserver", { writable: true, value: ResizeObserverMock });
